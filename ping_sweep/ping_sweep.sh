11447141e55a8ca3228b3f1f15eecb455addc828
Cyber Range Student <student@ip-10-1-150-98.ec2.internal>
2023-03-08 01:07:53 +0000

    Added the ping sweep

---
+++ ping_sweep/ping_sweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter the Subnet:"
+read Subnet
+if [ "Subnet" == "" ]
+then
+	echo "Enter the SUbnet:"
+	echo "Syntax Example = ./ping_sweep.sh 10.1.150"
+else
+
+	for IP in $(seq 1 254); do
+		ping -c 1 $Subnet.$IP | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> discoveredIPs_Anas.txt &
+done
+fi
